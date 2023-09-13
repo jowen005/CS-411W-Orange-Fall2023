d59e6bee58c955a5bdef4bbe7278038dd8394033
James <jowen005@odu.edu>
2023-09-13 19:44:50 -0400

    Added modlels to admin site

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import User
 
 # Register your models here.
+from .models import User
 admin.site.register(User)
+
+from .models import CustomUserManager
+admin.site.register(CustomUserManager)
--- restaurants/admin.py
+++ restaurants/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
-from . import models
 
 # Register your models here.
-admin.site.register(models.Restaurant)
-admin.site.register(models.RestaurantOpenHours)
-admin.site.register(models.RestTag)
+from .models import RestTag
+admin.site.register(RestTag)
+
+from .models import Restaurant
+admin.site.register(Restaurant)
+
+from .models import RestaurantOpenHours
+admin.site.register(RestaurantOpenHours)
+
+from .models import FoodTypeTag
+admin.site.register(FoodTypeTag)
+
+from .models import CookStyleTag
+admin.site.register(CookStyleTag)
+
+from .models import TasteTag
+admin.site.register(TasteTag)
+
+from .models import MenuItem
+admin.site.register(MenuItem)
