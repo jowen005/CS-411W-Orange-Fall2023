5c863a05867240b4f714ccf44db56e870d6b4f25
James <jowen005@odu.edu>
2023-09-13 20:42:27 -0400

    Fixed admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import User
-admin.site.register(User)
-
-from .models import CustomUserManager
-admin.site.register(CustomUserManager)
+#from .models import User
+#admin.site.register(User)
