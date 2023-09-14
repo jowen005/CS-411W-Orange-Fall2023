f24a214c53aba28113ecd9e2b6ca225628e86449
zjian001 <zjian001@odu.edu>
2023-09-14 18:33:24 -0400

    ratings auto update the avg rating to menuitem

--- feedback/admin.py
+++ feedback/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Ratings
+admin.site.register(Ratings)
+
+from .models import Reviews
+admin.site.register(Reviews)
--- feedback/models.py
+++ feedback/models.py
@@ -25,11 +25,27 @@ class Ratings(models.Model):
     patron = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE, null=True) 
     ratings = models.DecimalField(
-        max_digits=2,  # Total number of digits
-        decimal_places=1,  # Maximum of 2 decimal places
-        validators=[MinValueValidator(0.1)],  # Positive only
-        null=True,  # Allow null values
-        blank=True
+        choices = [('1', '1'), ('2', '2'), ('3', '3'),('4', '4'),('5', '5')],
+        max_digits=3, decimal_places=2
     )
+    rating_datetime = models.DateTimeField(auto_now_add=True)
+
+    def formatted_datetime(self):
+        return self.rating_datetime.strftime('%d/%m/%y %H:%M:%S')
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)  # Call the parent class's save method
+
+        # Calculate the new average rating for the associated menu item
+        menu_item = self.menu_item
+        ratings = Ratings.objects.filter(menu_item=menu_item)
+        total_ratings = sum(int(r.ratings) for r in ratings)
+        average_rating = total_ratings / len(ratings)
+
+        # Update the average_rating field in the MenuItem model
+        menu_item.average_rating = average_rating
+        menu_item.save()
+
     class Meta:
         db_table = 'Ratings'
+
