2039b4307e5e60cdd47970139998bf6acbe7c259
wtfoster <88130412+wtfoster@users.noreply.github.com>
2023-09-25 15:27:40 -0400

    abstract base classes for generate/load commands

--- restaurants/management/commands/generateRestaurants.py
+++ restaurants/management/commands/generateRestaurants.py
@@ -1,38 +1,15 @@
-from django.core.management.base import BaseCommand, CommandParser
-from restaurants.models import RestTag
-from django.contrib.auth import get_user_model
 from faker import Faker
-import random  # Import the random module
-import json
-from pathlib import Path
+import random
 
-DEFAULT_JSON_PATH = 'json_files/restaurantBuffer.json'
+from lcc_project.commands.generate import GenerateCommand, add_file_path
+from restaurants.models import RestTag
 
-User = get_user_model()
-class Command(BaseCommand):
-    help = 'Generate and insert fake patron user data'
-    def add_arguments(self, parser: CommandParser):
-        parser.add_argument("count", nargs=1, type=int)
-
-
-    def handle(self, count,*args, **kwargs):
-        
-        count = int(count[0])
-
-        gen_data = self.generate(count)
-        
-        #Export Data
-        APP_DIR = Path(__file__).resolve().parent.parent
-        with open(APP_DIR/DEFAULT_JSON_PATH, 'w') as outfile:
-            json.dump(gen_data, outfile, indent=4)
-
-        self.stdout.write(self.style.SUCCESS(f'{count} Object(s) were just generated'))
-
+@add_file_path
+class Command(GenerateCommand):
+    DEFAULT_JSON_PATH = 'json_files/restaurantBuffer.json'
 
     def generate(self, count:int):
-        
-        fake = Faker()
-        
+
         #Define Valid Value Constants
         valid_vb_zip_codes = [
             '23450', '23451', '23452', '23453', '23454',
@@ -43,7 +20,7 @@ class Command(BaseCommand):
         ]
 
         #Get Valid Values for PK Field attributes
-        valid_owners = list(User.objects.filter(user_type='restaurant').values_list('id',flat=True))
+        valid_owners = list(self.User.objects.filter(user_type='restaurant').values_list('id',flat=True))
         valid_tags = list(RestTag.objects.values_list('id',flat=True))
         
         data_list = []
@@ -52,12 +29,12 @@ class Command(BaseCommand):
             data = {
                 "owner": random.choice(valid_owners),
                 "name": "A Restaurant Name",
-                "rating": float(fake.numerify(text='#.##')),
-                "tags": list(set(fake.random_choices(elements=valid_tags))),
-                "price_level": fake.random_element(elements=('$', '$$', '$$$')),
-                "phone_number": fake.numerify(text='757-###-####'),
-                "website": fake.url(),
-                "street_name": fake.street_address(),
+                "rating": float(self.fake.numerify(text='#.##')),
+                "tags": list(set(self.fake.random_choices(elements=valid_tags))),
+                "price_level": self.fake.random_element(elements=('$', '$$', '$$$')),
+                "phone_number": self.fake.numerify(text='757-###-####'),
+                "website": self.fake.url(),
+                "street_name": self.fake.street_address(),
                 "city": "Virginia Beach",
                 "state": "VA",
                 "zip_code": random.choice(valid_vb_zip_codes),
@@ -66,5 +43,3 @@ class Command(BaseCommand):
             data_list.append(data)
 
         return data_list
-
-    
--- restaurants/management/commands/loadRestaurants.py
+++ restaurants/management/commands/loadRestaurants.py
@@ -1,38 +1,10 @@
-from django.core.management.base import BaseCommand, CommandParser
 from restaurants.models import Restaurant, RestTag
-from django.contrib.auth import get_user_model
-import json
-from pathlib import Path
-
-DEFAULT_JSON_PATH = 'json_files/restaurantBuffer.json'
-
-User = get_user_model()
-class Command(BaseCommand):
-    help = 'Generate and insert fake patron user data'
-    def add_arguments(self, parser: CommandParser):
-        parser.add_argument('-f', dest='json_path', default=DEFAULT_JSON_PATH, 
-                            help='Specifies a file to load',)
+from lcc_project.commands.load import LoadCommand, add_file_path
 
 
-    def handle(self, *args, **options):
-        
-        if options['json_path'] == DEFAULT_JSON_PATH:
-            APP_DIR = Path(__file__).resolve().parent.parent
-            json_path = APP_DIR + options['json_path']
-        else:
-            json_path = options['json_path']
-        
-        #Input Data
-        try:
-            with open(APP_DIR/json_path) as infile:
-                data_list = json.load(infile)
-        except IOError:
-            print("ERROR")
-            data_list = {}
-
-        self.load(data_list)
-
-        self.stdout.write(self.style.SUCCESS(f'All Objects were successfully loaded'))
+@add_file_path
+class Command(LoadCommand):
+    DEFAULT_JSON_PATH = 'json_files/restaurantBuffer.json'
 
 
     def load(self, data_list):
@@ -42,7 +14,7 @@ class Command(BaseCommand):
             tag_ids = obj.pop('tags')
             
             #Retrieve objects references by PKFields
-            owner = User.objects.get(pk=owner_id)   # Format for ForeignKey/OneToOneFields
+            owner = self.User.objects.get(pk=owner_id)   # Format for ForeignKey/OneToOneFields
             tags = [RestTag.objects.get(pk=tag_id) for tag_id in tag_ids]   #Format for ManyToManyFields
 
             #Create object
