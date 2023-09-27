104d4180b5828114e6ad09c82c57ff737451da2b
joberodu <jober007@odu.edu>
2023-09-27 19:20:29 -0400

    modified:   restaurants/management/commands/loadMenuTags.py     modified:   restaurants/management/json_files/menutags.json

--- restaurants/management/commands/loadMenuTags.py
+++ restaurants/management/commands/loadMenuTags.py
@@ -1,4 +1,4 @@
-from restaurants.models import FoodTypeTag,CookStyleTag,TasteTag,Allergy_tag,IngredientsTag,Restriction_tag
+from restaurants.models import FoodTypeTag,CookStyleTag,TasteTag,AllergyTag,IngredientTag,RestrictionTag
   
 from lcc_project.commands.load import LoadCommand, add_file_path
 
@@ -14,20 +14,20 @@ class Command(LoadCommand):
         #clearing the tag tables before load, don't want a evergrownig table of duplicates!
         FoodTypeTag.objects.all().delete()
         CookStyleTag.objects.all().delete()
-        Allergy_tag.objects.all().delete()
+        AllergyTag.objects.all().delete()
         TasteTag.objects.all().delete()
-        Restriction_tag.objects.all().delete()
-        IngredientsTag.objects.all().delete()
+        RestrictionTag.objects.all().delete()
+        IngredientTag.objects.all().delete()
 
         for el in TAGS['resttags']['FoodTypeTag']:
             FoodTypeTag.objects.create(title=str(el))
         for el in TAGS['resttags']['CookStyleTag']:
             CookStyleTag.objects.create(title=str(el))
-        for el in TAGS['resttags']['Allergy_tag']:  
-            Allergy_tag.objects.create(title=str(el))
+        for el in TAGS['resttags']['AllergyTag']:  
+            AllergyTag.objects.create(title=str(el))
         for el in TAGS['resttags']['TasteTag']:
             TasteTag.objects.create(title=str(el))
-        for el in TAGS['resttags']['Restriction_tag']:
-            Restriction_tag.objects.create(title=str(el))
-        for el in TAGS['resttags']['IngredientsTag']:
-            IngredientsTag.objects.create(title=str(el))
+        for el in TAGS['resttags']['RestrictionTag']:
+            RestrictionTag.objects.create(title=str(el))
+        for el in TAGS['resttags']['IngredientTag']:
+            IngredientTag.objects.create(title=str(el))
