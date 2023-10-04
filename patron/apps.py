86ce305004decbf135d562f1fb1919102ae04d75
joberodu <jober007@odu.edu>
2023-10-04 17:21:26 -0400

    deleted extrainous files

--- patron/apps.py
+++ patron/apps.py
@@ -1,6 +1,18 @@
 from django.apps import AppConfig
-
+from patron.models import Patron
+from restaurant.models import #tags? menu items? I think I need from here
 
 class PatronConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'patron'
+
+class SearchHandler:
+    def preformQuickSearch(patron):
+        #predefine all the advanced search options
+        #call advanced search
+        pass
+    def preformAdvancedSearch(patron):
+        #load patron details
+        #generate SQL query.
+        #send query to database.
+        #return results list. 
