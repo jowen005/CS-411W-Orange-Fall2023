9ef432893a11abbb467807010a4d5a7985d3e6e9
joberodu <jober007@odu.edu>
2023-10-04 17:46:27 -0400

    Quick update to Apps

--- patron/apps.py
+++ patron/apps.py
@@ -1,11 +1,11 @@
 from django.apps import AppConfig
 from patron.models import Patron
-from restaurant.models import #tags? menu items? I think I need from here
+from restaurant.models import MenuItem#tags? menu items? I think I need from here
 
 class PatronConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'patron'
-
+    
 class SearchHandler:
     def preformQuickSearch(patron):
         #predefine all the advanced search options
@@ -16,3 +16,4 @@ class SearchHandler:
         #generate SQL query.
         #send query to database.
         #return results list. 
+        pass
