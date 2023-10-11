ea12f91b6bd2d926882c7df9ba631a5381961e96
James <jowen005@odu.edu>
2023-10-11 00:02:53 -0400

    First test

--- patron/tests.py
+++ patron/tests.py
@@ -1,9 +1,13 @@
 from rest_framework.test import APITestCase
 from rest_framework.test import APIRequestFactory
 from django.urls import reverse
+from rest_framework import status
 # Test patron view set
 
 # Test patron tags
-#class PatronTagTestCase(APITestCase):
+class PatronTagTestCase(APITestCase):
     
-    #def
+    def test_tag_overview(self):
+        response = self.client.get(reverse('analytics'))
+
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
