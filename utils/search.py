daf133fbc6293ce7cabf7f38f98271e7cba5db81
joberodu <jober007@odu.edu>
2023-10-24 18:41:59 -0400

    Update search

--- utils/search.py
+++ utils/search.py
@@ -2,15 +2,29 @@ from restaurants.models import MenuItem
 from restaurants.models import Patron
 
 def quickSearch(patron_ID, query_string):
-    pass
+    patron = Patrons.objects.get(patron_ID)
+    callorie_range = (0,patron.calorie_limit)
+    if(patron.price_preference = '$'):
+        price_max = 25
+    if(patron.price_preference = '$$'):
+        price_max = 50
+    if(patron.price_preference = '$$$'):
+        price_max = 100
+    restriction_tags = list(patron.patron_restriction_tag)
+    allergy_tags = list(patron.patron_allergy_tag)
+    excluded_ingredient_tags = list(patron.disliked_ingredients)
+    taste_tags = list(patron.patron_taste_tag)
+
+    return advancedSearch(query_string ,callorie_range,price_max,restriction_tags,allergy_tags,excluded_ingredient_tags,taste_tags)
+
 
 #this function will hit the database twice, once to pull the patron profile and again with the actual search query.
 #since all the values set by the patron profile are specified in an advanced search patron_ID is not provided
 #
 #returns a list of menu item IDs matching the search
-def advancedSearch(query_string:str, callorie_range:tuple, price_range:tuple,
-                   restriction_tags:list, allergy_tags:list, excluded_ingredient_tags:list,
-                   taste_tags:list, style:list, time:datetime, sortMethod:int):
+def advancedSearch(query_string:str, callorie_range:tuple=(0,0), price_max:float=0f,
+                   restriction_tags:list=None, allergy_tags:list=None, excluded_ingredient_tags:list=None,
+                   taste_tags:list=None, style:list=None, time:datetime=None, sortMethod:int=0):
     #select from menuitems where restriction_tags is true and allergy_tags is false and excluded_ingredient_tags is false and taste_tags is true and style = style and resturant_hours < time 
     
     #acording to the django docs the database shouldn't be queried until the query object is evaluated so we should be able to stack filters without
@@ -21,61 +35,6 @@ def advancedSearch(query_string:str, callorie_range:tuple, price_range:tuple,
     #get day of week from time.day
     #big ol' if block
     #Restuarants = Resturant.objects.filter(open__gte=time,close__lte=time)
-
-
-    #todo add nullable functionality for the optional sections.
-
-    #the most restrictive tag is likely the allergy tags so we'll filter on that first
-    MenuItems = MenuItem.objects.exclude(menu_allergy_tag__in = allergy_tags)
-    #current SQL query should look like SELECT * from MenuItems
-                                       #WHERE NOT (AllergyTag IN list(allergy_tags));
-    MenuItems = MenuItems.exclude(ingredients_tag__in = excluded_ingredient_tags)
-    #current SQL query should look like SELECT * from MenuItems
-                                       #WHERE NOT (AllergyTag IN list(allergy_tags));
-                                       #AND NOT(IngredientTag IN list(excluded_ingredient_tags))
-
-    #note to self this may need to be reworked as a loop to ensure that ALL restriction tags are match
-    #todo test the above theory.
-    MenuItems = MenuItems.filter(menu_restriction_tag__in = restriction_tags)
-    #current SQL query should look like SELECT * from MenuItems
-                                       #WHERE NOT (AllergyTag IN list(allergy_tags));
-                                       #AND NOT(IngredientTag IN list(excluded_ingredient_tags))
-                                       #AND (RestrictionTag IN list(restriction_Tags))
-    MenuItems = MenuItems.filter(taste_tags__in = taste_tags)
-    #current SQL query should look like SELECT * from MenuItems
-                                       #WHERE NOT (AllergyTag IN list(allergy_tags));
-                                       #AND NOT(IngredientTag IN list(excluded_ingredient_tags))
-                                       #AND (RestrictionTag IN list(restriction_Tags))
-                                       #AND (taste_tags IN list(taste_tags))
-
-    #menu items can only have one style tag so this shouldn't need the above rework.
-    MenuItems = MenuItems.filter(cook_style_tags__in = style)
-    #current SQL query should look like SELECT * from MenuItems
-                                       #WHERE NOT (AllergyTag IN list(allergy_tags));
-                                       #AND NOT(IngredientTag IN list(excluded_ingredient_tags))
-                                       #AND (RestrictionTag IN list(restriction_Tags))
-                                       #AND (TasteTag IN list(taste_tags))
-                                       #AND (cook_style_tags IN list(style))
-    MenuItems = MenuItems.filter(price__range = price_range)
-    #current SQL query should look like SELECT * from MenuItems
-                                       #WHERE NOT (AllergyTag IN list(allergy_tags));
-                                       #AND NOT(IngredientTag IN list(excluded_ingredient_tags))
-                                       #AND (RestrictionTag IN list(restriction_Tags))
-                                       #AND (TasteTag IN list(taste_tags))
-                                       #AND (CookStyleTag IN list(style))
-                                       #AND price BETWEEN price_range[0] and price_range[1];
-    MenuItems = MenuItems.filter(calories__range = callorie_range)
-    #current SQL query should look like SELECT * from MenuItems
-                                       #WHERE NOT (AllergyTag IN list(allergy_tags))
-                                       #AND NOT(IngredientTag IN list(excluded_ingredient_tags))
-                                       #AND (RestrictionTag IN list(restriction_Tags))
-                                       #AND (TasteTag IN list(taste_tags))
-                                       #AND (CookStyleTag IN list(style))
-                                       #AND price BETWEEN price_range[0] and price_range[1]
-                                       #AND calories BETWEEN callorie_range[0] and callorie_range[1]
-
-    #now it gets complicated and hard to track what the query should look like                                
-    #theoretically we can save the query string until last weirdly
     for queryElement in split(query_string, " ")
         #input should be clean before getting here but JUST IN CASE we'll do a little input sanitization
         queryElement = queryElement.replace('\"','')
@@ -87,6 +46,68 @@ def advancedSearch(query_string:str, callorie_range:tuple, price_range:tuple,
             MenuItems = MenuItems.objects.exclude(name__icontains = queryElement)
         else:
             MenuItems = MenuItems.objects.filter(name__icontains = queryElement)
+    
+
+    #todo add nullable functionality for the optional sections.
+
+    #the most restrictive tag is likely the allergy tags so we'll filter on that first
+    if allergy_tags is not None:
+        MenuItems = MenuItem.objects.exclude(menu_allergy_tag__in = allergy_tags)
+        #current SQL query should look like SELECT * from MenuItems
+                                        #WHERE NOT (AllergyTag IN list(allergy_tags));
+    if excluded_ingredient_tags is not None:
+        MenuItems = MenuItems.exclude(ingredients_tag__in = excluded_ingredient_tags)
+        #current SQL query should look like SELECT * from MenuItems
+                                           #WHERE NOT (AllergyTag IN list(allergy_tags));
+                                           #AND NOT(IngredientTag IN list(excluded_ingredient_tags))
+
+        #note to self this may need to be reworked as a loop to ensure that ALL restriction tags are match
+       
+        #todo test the above theory.
+    if restriction_tags is not None:
+        MenuItems = MenuItems.filter(menu_restriction_tag__in = restriction_tags)
+        #current SQL query should look like SELECT * from MenuItems
+                                        #WHERE NOT (AllergyTag IN list(allergy_tags));
+                                        #AND NOT(IngredientTag IN list(excluded_ingredient_tags))
+                                        #AND (RestrictionTag IN list(restriction_Tags))
+    if taste_tags is not None:
+        MenuItems = MenuItems.filter(taste_tags__in = taste_tags)
+        #current SQL query should look like SELECT * from MenuItems
+                                        #WHERE NOT (AllergyTag IN list(allergy_tags));
+                                        #AND NOT(IngredientTag IN list(excluded_ingredient_tags))
+                                        #AND (RestrictionTag IN list(restriction_Tags))
+                                        #AND (taste_tags IN list(taste_tags))
+    if style is not None:
+        #menu items can only have one style tag so this shouldn't need the above rework.
+        MenuItems = MenuItems.filter(cook_style_tags__in = style)
+        #current SQL query should look like SELECT * from MenuItems
+                                        #WHERE NOT (AllergyTag IN list(allergy_tags));
+                                        #AND NOT(IngredientTag IN list(excluded_ingredient_tags))
+                                        #AND (RestrictionTag IN list(restriction_Tags))
+                                        #AND (TasteTag IN list(taste_tags))
+                                        #AND (cook_style_tags IN list(style))
+    if price_range is not None:        
+        MenuItems = MenuItems.filter(price__range = price_range)
+        #current SQL query should look like SELECT * from MenuItems
+                                        #WHERE NOT (AllergyTag IN list(allergy_tags));
+                                        #AND NOT(IngredientTag IN list(excluded_ingredient_tags))
+                                        #AND (RestrictionTag IN list(restriction_Tags))
+                                        #AND (TasteTag IN list(taste_tags))
+                                        #AND (CookStyleTag IN list(style))
+                                        #AND price BETWEEN price_range[0] and price_range[1];
+    if callorie_range is not None:        
+        MenuItems = MenuItems.filter(calories__range = callorie_range)
+        #current SQL query should look like SELECT * from MenuItems
+                                        #WHERE NOT (AllergyTag IN list(allergy_tags))
+                                        #AND NOT(IngredientTag IN list(excluded_ingredient_tags))
+                                        #AND (RestrictionTag IN list(restriction_Tags))
+                                        #AND (TasteTag IN list(taste_tags))
+                                        #AND (CookStyleTag IN list(style))
+                                        #AND price BETWEEN price_range[0] and price_range[1]
+                                        #AND calories BETWEEN callorie_range[0] and callorie_range[1]
+
+    #now it gets complicated and hard to track what the query should look like                                
+    #theoretically we can save the query string until last weirdly
     
     #for debug purposes remove when finished
     print(MenuItems.query)
