353f75a2bddfe58a4b27bf1e8ac1652bb17386b7
James <jowen005@odu.edu>
2023-12-01 02:08:09 -0500

    registered analytics models

--- analytics/admin.py
+++ analytics/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import GlobalAnalytics
+admin.site.register(GlobalAnalytics)
+
+from .models import CalorieAnalytics
+admin.site.register(CalorieAnalytics)
+
+from .models import RestrictionTagAnalytics
+admin.site.register(RestrictionTagAnalytics)
+
+from .models import AllergiesTagAnalytics
+admin.site.register(AllergiesTagAnalytics)
+
+from .models import IngredientTagAnalytics
+admin.site.register(IngredientTagAnalytics)
+
+from .models import TasteTagAnalytics
+admin.site.register(TasteTagAnalytics)
+
+from .models import CookStyleAnalytics
+admin.site.register(CookStyleAnalytics)
+
+from .models import MenuItemPerformanceAnalytics
+admin.site.register(MenuItemPerformanceAnalytics)
+
+from .models import AppSatisfactionAnalytics
+admin.site.register(AppSatisfactionAnalytics)
+
+from .models import LocalRestaurantAnalytics
+admin.site.register(LocalRestaurantAnalytics)
