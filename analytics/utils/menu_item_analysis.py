63d95801fd135b34e8a33bfb42180d03cdef4316
wtfoster <88130412+wtfoster@users.noreply.github.com>
2023-12-01 00:50:11 -0500

    fix exclusions

--- analytics/utils/menu_item_analysis.py
+++ analytics/utils/menu_item_analysis.py
@@ -10,26 +10,13 @@ from ..models import (AllergyTagExclusionRecord, IngredientTagExclusionRecord,
                       RestrictionTagExclusionRecord, TasteTagExclusionRecord,
                       OverallExclusionRecord)
 
-import tag_exclusion
-
 # Number of searches Menu Items were excluded from (total since)
 
 def driver():
 
-    try:
-        latest_datestamp = MenuItemPerformanceAnalytics.objects.latest('date_stamp').date_stamp
-    except MenuItemPerformanceAnalytics.DoesNotExist:
-        latest_datestamp = datetime.utcfromtimestamp(0).replace(tzinfo=timezone.utc)
+    item_data, current_datestamp = item_analysis()
 
-    menu_items = rm.MenuItem.objects.all().order_by('id')
-    searches = pm.PatronSearchHistory.filter(search_datetime__gte=latest_datestamp)
-    bookmark_set = pm.Bookmark.objects.filter(bookmarked_datetime__gt=latest_datestamp)
-    history_set = pm.MenuItemHistory.objects.filter(MenuItemHS_datetime__gt=latest_datestamp)
-    review_set = Reviews.objects.all()
-    current_datestamp = timezone.now()
-
-    item_data = item_analysis(menu_items, bookmark_set, history_set, review_set)
-    item_data = tag_exclusion.driver(item_data, menu_items, searches)
+    item_data = exclusion_analysis(item_data)
 
     for entry in item_data:
         # print(f'{current_datestamp} | {entry}') #NOTE
@@ -39,25 +26,20 @@ def driver():
         update_menu_item(entry)
     print('\n')
 
-
-def update_menu_item(entry):
-    item_instance = entry['menuItem_id']
-    
-    item_instance.number_of_rating = entry['number_of_ratings']
-    item_instance.average_rating = entry['average_rating']
-    item_instance.save()
-
-
 def item_analysis(items, bookmark_set, history_set, review_set):
-    # items = rm.MenuItem.objects.all().order_by('id') #filter(discontinued=False)
+    
+    try:
+        latest_datestamp = MenuItemPerformanceAnalytics.objects.latest('date_stamp').date_stamp
+    except MenuItemPerformanceAnalytics.DoesNotExist:
+        latest_datestamp = datetime.utcfromtimestamp(0).replace(tzinfo=timezone.utc)
+    
     item_data = []
 
-    
-
-    # bookmark_set = pm.Bookmark.objects.filter(bookmarked_datetime__gt=latest_datestamp)
-    # history_set = pm.MenuItemHistory.objects.filter(MenuItemHS_datetime__gt=latest_datestamp)
-    # review_set = Reviews.objects.all()
-    # current_datestamp = timezone.now()
+    items = rm.MenuItem.objects.all().order_by('id')
+    bookmark_set = pm.Bookmark.objects.filter(bookmarked_datetime__gt=latest_datestamp)
+    history_set = pm.MenuItemHistory.objects.filter(MenuItemHS_datetime__gt=latest_datestamp)
+    review_set = Reviews.objects.all()
+    current_datestamp = timezone.now()
 
     for item in items:
         data = {}
@@ -76,4 +58,68 @@ def item_analysis(items, bookmark_set, history_set, review_set):
 
         item_data.append(data)
 
+    return item_data, current_datestamp
+
+
+def update_menu_item(entry):
+    item_instance = entry['menuItem_id']
+    
+    item_instance.number_of_rating = entry['number_of_ratings']
+    item_instance.average_rating = entry['average_rating']
+    item_instance.save()
+
+
+def exclusion_analysis(item_data):
+
+    TAG_TYPES = [('allergy', rm.AllergyTag, AllergyTagExclusionRecord), 
+                 ('ingredients', rm.IngredientTag, IngredientTagExclusionRecord), 
+                 ('restrictions', rm.RestrictionTag, RestrictionTagExclusionRecord),
+                 ('taste', rm.TasteTag, TasteTagExclusionRecord)]
+    
+    analytic_sets = {tag_type: AnalyticsModel.objects.all() for tag_type, _, AnalyticsModel in TAG_TYPES}
+    overall_set = OverallExclusionRecord.objects.all()
+
+    for entry in item_data:
+        menu_item = entry['menuItem_id']
+
+        # Overall Exclusion
+        entry['exclusion_count'] = overall_set.get(menu_item=menu_item).exclusion_count
+
+        for tag_type, *_ in TAG_TYPES:
+            top_3 = list(analytic_sets[tag_type].filter(
+                menu_item=menu_item
+            ).order_by('-exclusion_count')[:3])
+
+            entry[f'top_3_{tag_type}'] = {
+                f'{idx+1}': {
+                    'title':record.tag.title,
+                    'count':record.exclusion_count
+                } for idx, record in enumerate(top_3)
+            }
+
     return item_data
+
+
+
+# # Store exclusion
+    # for entry in item_data:
+    #     menu_item = entry['menuItem_id']
+
+    #     #Overall Exclusion
+    #     entry['exclusion_count'] = overall_exclusions[f'{menu_item.id}'].count
+
+    #     for tag_type, *_ in TAG_TYPES:
+    #         tag_counts = tag_exclusions[tag_type][f'{menu_item.id}']
+    #         sorted_tags = sorted(tag_counts.items(), key=lambda x: x[1], reverse=True)
+    #         top_3 = sorted_tags[:3]
+            
+    #         idx, tag_dict = 1, {}
+    #         for tag_id, count in top_3:
+    #             tag_dict[f'{idx}'] = {
+    #                 'title':tag_sets[tag_type].get(id=tag_id).title,
+    #                 'count':count
+    #             }
+    #         entry[f'top_3_{tag_type}'] = tag_dict
+
+    # return item_data
+                
--- analytics/utils/tag_exclusion.py
+++ analytics/utils/tag_exclusion.py
@@ -4,29 +4,32 @@ from dataclasses import dataclass
 
 import restaurants.models as rm
 import patron.models as pm
+from ..models import (AllergyTagExclusionRecord, IngredientTagExclusionRecord,
+                      RestrictionTagExclusionRecord, TasteTagExclusionRecord,
+                      OverallExclusionRecord) #Only Stores Most Recent Info
 
 @dataclass
 class Exclusion:
     count: int = 0
     is_excluded: bool = False
 
-def driver(item_data, menu_items, searches):
+def driver():
 
-    # try:
-    #     latest_datestamp = OverallExclusionRecord.objects.latest('date_stamp').date_stamp
-    # except OverallExclusionRecord.DoesNotExist:
-    #     latest_datestamp = datetime.utcfromtimestamp(0).replace(tzinfo=timezone.utc)
+    try:
+        latest_datestamp = OverallExclusionRecord.objects.latest('date_stamp').date_stamp
+    except OverallExclusionRecord.DoesNotExist:
+        latest_datestamp = datetime.utcfromtimestamp(0).replace(tzinfo=timezone.utc)
     
-    # searches = pm.PatronSearchHistory.filter(search_datetime__gte=latest_datestamp)
-    # menu_items = rm.MenuItem.objects.all().order_by('id')
-    # current_datestamp = timezone.now()
+    searches = pm.PatronSearchHistory.filter(search_datetime__gte=latest_datestamp)
+    menu_items = rm.MenuItem.objects.all().order_by('id')
+    current_datestamp = timezone.now()
     
-    TAG_TYPES = [('allergy', rm.AllergyTag), 
-                 ('ingredients', rm.IngredientTag), 
-                 ('restrictions', rm.RestrictionTag),
-                 ('taste', rm.TasteTag)]
+    TAG_TYPES = [('allergy', rm.AllergyTag, AllergyTagExclusionRecord), 
+                 ('ingredients', rm.IngredientTag, IngredientTagExclusionRecord), 
+                 ('restrictions', rm.RestrictionTag, RestrictionTagExclusionRecord),
+                 ('taste', rm.TasteTag, TasteTagExclusionRecord)]
     
-    tag_sets = {tag_type: TagModel.objects.all() for tag_type, TagModel in TAG_TYPES}
+    tag_sets = {tag_type: TagModel.objects.all() for tag_type, TagModel, *_ in TAG_TYPES}
 
     tag_exclusions = {
         tag_type: {
@@ -46,7 +49,6 @@ def driver(item_data, menu_items, searches):
                 tag_exclusions['allergy'][f'{item.id}'][f'{allergy.id}'] += 1
                 if not overall_exclusions[f'{item.id}'].is_excluded:
                     overall_exclusions[f'{item.id}'].count += 1
-
 
         # Excluded if item has any of the disliked ingredients (record tags that led to it getting excluded)
         for ingredient in search.disliked_ingredients.all():
@@ -76,64 +78,79 @@ def driver(item_data, menu_items, searches):
         for item in menu_items:
             overall_exclusions[f'{item.id}'].is_excluded = False
 
+    analytic_sets = {tag_type: AnalyticsModel.objects.all() for tag_type, _, AnalyticsModel in TAG_TYPES}
+    overall_set = OverallExclusionRecord.objects.all()
+
+    tag_records_to_update = []
+    tag_records_to_create = []
+    overall_to_update = []
+    overall_to_create = []
 
     # Store exclusion
-    for entry in item_data:
-        menu_item = entry['menuItem_id']
+    for tag_type, _, AnalyticsModel in TAG_TYPES:
+        for item in menu_items:
+            # Update Overall Exclusions
+            try:
+                record = overall_set.get(menu_item=item)
+                record.exclusion_count += overall_exclusions[f'{item.id}'].count
+                record.date_stamp = current_datestamp
+                record.save()
+                # overall_to_update.append(record)
 
-        #Overall Exclusion
-        entry['exclusion_count'] = overall_exclusions[f'{menu_item.id}'].count
-
-        for tag_type, *_ in TAG_TYPES:
-            tag_counts = tag_exclusions[tag_type][f'{menu_item.id}']
-            sorted_tags = sorted(tag_counts.items(), key=lambda x: x[1], reverse=True)
-            top_3 = sorted_tags[:3]
+            except AnalyticsModel.DoesNotExist:
+                AnalyticsModel.objects.create(
+                    menu_item=item, 
+                    exclusion_count=overall_exclusions[f'{item.id}'].count, 
+                    date_stamp=current_datestamp
+                )
+                # overall_to_create.append(AnalyticsModel(
+                #     menu_item=item, 
+                #     exclusion_count=overall_exclusions[f'{item.id}'].count, 
+                #     date_stamp=current_datestamp
+                # ))
             
-            idx, tag_dict = 1, {}
-            for tag_id, count in top_3:
-                tag_dict[f'{idx}'] = {
-                    'title':tag_sets[tag_type].get(id=tag_id).title,
-                    'count':count
-                }
-            entry[f'top_3_{tag_type}'] = tag_dict
+            for tag in tag_sets[tag_type]:
+                # Update Tag Exclusions
+                try:
+                    tag_record = analytic_sets[tag_type].get(menu_item=item, tag=tag)
+                    tag_record.exclusion_count += tag_exclusions[tag_type][f'{item.id}'][f'{tag.id}']
+                    tag_record.date_stamp = current_datestamp
+                    tag_record.save()
+                    # tag_records_to_update.append(record)
 
-    return item_data
-                
-
+                except AnalyticsModel.DoesNotExist:
+                    AnalyticsModel.objects.create(
+                        menu_item=item, 
+                        tag=tag, 
+                        exclusion_count=tag_exclusions[tag_type][f'{item.id}'][f'{tag.id}'], 
+                        date_stamp=current_datestamp
+                    )
+                    # tag_records_to_create.append(AnalyticsModel(
+                    #     menu_item=item, 
+                    #     tag=tag, 
+                    #     exclusion_count=tag_exclusions[tag_type][f'{item.id}'][f'{tag.id}'], 
+                    #     date_stamp=current_datestamp
+                    # ))
         
-            
-            
-    # analytic_sets = {tag_type: AnalyticsModel.objects.all() for tag_type, _, AnalyticsModel in TAG_TYPES}
-    # overall_set = OverallExclusionRecord.objects.all()
+    #     AnalyticsModel.objects.bulk_update(tag_records_to_update)
+    #     AnalyticsModel.objects.bulk_create(tag_records_to_create)
 
-    # # Store exclusion
-    # for tag_type, _, AnalyticsModel in TAG_TYPES:
-    #     for item in menu_items:
-    #         try:
-    #             record = overall_set.get(menu_item=item)
-    #             record.exclusion_count += overall_exclusions[f'{item.id}'].count
-    #             record.date_stamp = current_datestamp
-    #             record.save()
+    # OverallExclusionRecord.objects.bulk_update(overall_to_update)
+    # OverallExclusionRecord.objects.bulk_create(overall_to_create)
 
-    #         except AnalyticsModel.DoesNotExist:
-    #             AnalyticsModel.objects.create(
-    #                 menu_item=item, 
-    #                 exclusion_count=overall_exclusions[f'{item.id}'].count, 
-    #                 date_stamp=current_datestamp
-    #             )
-            
-    #         for tag in tag_sets[tag_type]:
-    #             try:
-    #                 tag_record = analytic_sets[tag_type].get(menu_item=item, tag=tag)
-    #                 tag_record.exclusion_count += tag_exclusions[tag_type][f'{item.id}'][f'{tag.id}']
-    #                 tag_record.date_stamp = current_datestamp
-    #                 tag_record.save()
 
-    #             except AnalyticsModel.DoesNotExist:
-    #                 AnalyticsModel.objects.create(
-    #                     menu_item=item, 
-    #                     tag=tag, 
-    #                     exclusion_count=tag_exclusions[tag_type][f'{item.id}'][f'{tag.id}'], 
-    #                     date_stamp=current_datestamp
-    #                 )
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
