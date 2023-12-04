fb79de7d8fb1eddf7c1caf87a5a41fd4c4c10827
wtfoster <88130412+wtfoster@users.noreply.github.com>
2023-12-03 23:36:27 -0500

    Fix Feedback API

--- feedback/views.py
+++ feedback/views.py
@@ -1,7 +1,7 @@
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, APIView
-from rest_framework import status, viewsets
+from rest_framework import status, viewsets, generics
 
 from . import models, serializers, permissions
 from restaurants.models import MenuItem
@@ -17,24 +17,48 @@ class ReviewViewSet(viewsets.ModelViewSet):
         user = self.request.user
 
         # queries for reviews for a specified menu item
-        if self.action == 'list':
-            menu_item = MenuItem.objects.get(id=self.request.data['menu_item'])
-            return models.Reviews.objects.filter(menu_item=menu_item)
+        # if self.action == 'list':
+        #     menu_item = MenuItem.objects.get(id=self.request.data['menu_item'])
+        #     return models.Reviews.objects.filter(menu_item=menu_item)
         
         # queries for reviews for a specific patron
-        elif user.user_type == 'patron':
+        if user.user_type == 'patron':
             return models.Reviews.objects.filter(patron=user)
-        
         else:
             return models.Reviews.objects.none()
         
     def get_serializer_class(self):
-        if self.action == 'list' or self.action == 'retrieve':
+        if self.action == 'retrieve':
             return serializers.ReviewsGetSerializer
         return serializers.ReviewsSerializer
 
     def perform_create(self, serializer):
         serializer.save(patron=self.request.user)
+
+
+class RetrieveMenuItemReviewsAPIView(generics.RetrieveAPIView):
+    permission_classes = [permissions.IsAuth]
+    serializer_class = serializers.ReviewsGetSerializer
+    queryset = models.Reviews.objects.all()
+
+    def retrieve(self, request, *args, **kwargs):
+        item_id = int(kwargs.get('pk'))
+
+        # Validate PK Input
+        try:
+            MenuItem.objects.get(id=item_id)
+        except MenuItem.DoesNotExist:
+            response = {
+                'message': f'This Menu Item ID ({item_id}) is not valid!'
+            }
+            return Response(data=response, status=status.HTTP_400_BAD_REQUEST)
+
+        queryset = self.get_queryset()
+        reviews = queryset.filter(menu_item__id=item_id).order_by('-review_datetime')
+
+        serializer = self.get_serializer(reviews, many=True)
+
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
 
 
 class AppSatisfactionViewSet(viewsets.ModelViewSet):
